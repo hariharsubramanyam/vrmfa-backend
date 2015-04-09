e45f9338270f7aa5dbf7e3abb1232bff30c4ad5c
Harihar Subramanyam <hahasubbu@gmail.com>
2015-04-08 23:59:09 -0400

    Multithreaded scraper

--- src/deviantart/scraper.py
+++ src/deviantart/scraper.py
@@ -1,3 +1,6 @@
+import time
+import threading
+
 '''
 The web scraper which pulls image data from DeviantArt.
 '''
@@ -16,6 +19,77 @@ class Scraper:
         self.callback = callback
         self.ping_interval = ping_interval
         self.max_imagedata_in_mem = max_imagedata_in_mem
+
+        # We want to avoid the current thread and the scrape loop thread from modifying the
+        # thread_running and ticks_until_next_scrape variables at the same time.
+        # So, they'll acquire this mutex before the modify, and they'll release it when they're done.
+        self.mutex = threading.Lock()
+
+        # When false, the scrape loop will terminate (and thus its thread will terminate).
+        self.thread_running = False
+
+        # The thread running the loop for the scraper.
+        self.thread = threading.Thread(target=self.scrape_loop)
+
+        # The scrape loop runs every second (a tick). This is the number of ticks it must
+        # wait until it can scrape DeviantArt again.
+        self.ticks_until_next_scrape = ping_interval
+
+
+    '''
+    The loop that will scrape periodically.
+    '''
+    def scrape_loop(self):
+        while True:
+            # Check if we should return (and therefore kill the thread).
+            self.mutex.acquire()
+            if not self.thread_running:
+                self.mutex.release()
+                return
+
+            # Determine if we should scrape on this tick.
+            self.ticks_until_next_scrape -= 1
+            if self.ticks_until_next_scrape == 0:
+                shouldScrape = True
+                self.ticks_until_next_scrape = self.ping_interval
+            else:
+                shouldScrape = False
+            self.mutex.release()
+
+            # Scrape if needed, then sleep until the next tick.
+            if shouldScrape:
+                self.scrape()
+            time.sleep(1)
+
+    '''
+    Scrape DeviantArt, parse the HTML to get the list of ImageData, and then call the callback.
+    '''
+    def scrape(self):
+        #TODO(Harihar): Actually implement something here.
+        print "Hello world"
+
+    '''
+    Run the scraper. This will launch another thread, so make sure to terminate it by calling
+    stop().
+    '''
+    def run(self):
+        self.mutex.acquire()
+        if self.thread_running:
+            self.mutex.release()
+            return
+        self.thread_running = True
+        self.ticks_until_next_scrape = self.ping_interval
+        self.mutex.release()
+        self.thread.start()
+
+    '''
+    Kill the scraper, if it is currently running.
+    '''
+    def stop(self):
+        self.mutex.acquire()
+        self.thread_running = False
+        self.mutex.release()
+
     '''
     Return string representation of this object.
     '''
--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
+'''
+Main entrypoint to the application.
+'''
+
 from models.imagedata import ImageData 
 from deviantart.scraper import Scraper
 
+import signal
+import sys
+
 if __name__=="__main__":
-    def default_callback(image_data_items):
-        for image_data in image_data_items:
-            print image_data
-    print Scraper(default_callback)
+    '''
+    When we receive a kill signal (Ctrl-C on UNIX system and Ctrl-C or Ctrl-BREAK on Windows),
+    stop the scraper.
+    '''
+    def signal_handler(signal, frame):
+        scraper.stop()
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+
+    # Create the scraper, the callback is currently a function that does nothing.
+    #TODO(Harihar): The callback should write the ImageData to a database.
+    scraper = Scraper(lambda x: x, ping_interval=1)
+
+    # Run the scraper.
+    scraper.run()
+
+    # Setup the signal handler to wait for the kill signal.
+    signal.signal(signal.SIGINT, signal_handler)
+    print('Press Ctrl+C')
+    signal.pause()
