bfa6e989e63ecd5981a898b3f11decd40e9c86ad
Harihar Subramanyam <hahasubbu@gmail.com>
2015-04-19 14:33:02 -0400

    Comments

--- src/roomgen/room_persist.py
+++ src/roomgen/room_persist.py
@@ -1,15 +1,31 @@
 from room import *
 import psycopg2
 
+'''
+Helper class for storing and retrieving rooms from a Postgres database.
+
+Make sure you call start() to begin operation and that you call finish() when you are done.
+'''
 class RoomPersist:
+    '''
+    Create the object with the given database and user. This constructor doesn't actually 
+    create the connnection. You need to call start() to create the connection (and finish() will
+    end the connection).
+    '''
     def __init__(self, dbname, user):
         self.dbname = dbname
         self.user = user
         self.conn = None
         self.cursor = None
+    '''
+    Connect to the databse.
+    '''
     def start(self):
         self.conn = psycopg2.connect("dbname=%s user=%s" % (self.dbname, self.user))
         self.cursor = self.conn.cursor()
+    '''
+    Create the tables. If they already exist, this will delete them and recreate them.
+    '''
     def create_tables(self):
         self.cursor.execute("DROP TABLE IF EXISTS walls;")
         self.cursor.execute("DROP TABLE IF EXISTS rooms;")
@@ -17,6 +33,10 @@ class RoomPersist:
         self.cursor.execute("CREATE TABLE walls (room_id uuid references rooms(id), startx real, starty real, endx real, endy real);")
         self.cursor.execute("CREATE INDEX room_id_idx ON walls(room_id);")
         self.conn.commit()
+    '''
+    Persist the room in the database. If the room is already in the database, it will be deleted
+    and then inserted.
+    '''
     def persist_room(self, room):
         # Get rid of the room if it exists.
         self.cursor.execute("DELETE FROM walls where room_id = '%s';" % (room.room_id,))
@@ -36,17 +56,27 @@ class RoomPersist:
         wall_query = wall_query_fmt % (", ".join(wall_strings))
         self.cursor.execute(wall_query)
         self.conn.commit()
+    '''
+    Return all the rooms with the given types. Their walls will not be retrieved, so you will need
+    to call set_walls_for_room(room) for each of the room objects populate their walls.
+    '''
     def get_no_wall_rooms_for_type(self, room_type):
         self.cursor.execute("SELECT * FROM rooms WHERE room_type = %s;" % (room_type,))
         self.conn.commit()
         tuples = self.cursor.fetchall()
         return [Room(t[2], t[1], room_id=t[0]) for t in tuples if len(t) == 3]
+    '''
+    Given a Room object, look up its walls in the database and populate the walls array.
+    '''
     def set_walls_for_room(self, room):
         self.cursor.execute("SELECT * FROM walls WHERE room_id = '%s';" % (room.room_id,))
         self.conn.commit()
         tuples = self.cursor.fetchall()
         walls = [Wall(t[1], t[2], t[3], t[4]) for t in tuples if len(t) == 5]
         room.walls = walls
+    '''
+    Close the connection to the database.
+    '''
     def finish(self):
         self.conn.commit()
         self.cursor.close()
